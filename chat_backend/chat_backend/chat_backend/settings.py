2d1fb2eadafe8d09c2ca467eb69ba2e326bf89c4
wlgud0402 <wlgudrlgus@naver.com>
2021-03-06 20:24:39 +0900

    rds settings

--- chat_backend/chat_backend/chat_backend/settings.py
+++ chat_backend/chat_backend/chat_backend/settings.py
@@ -78,14 +78,25 @@ WSGI_APPLICATION = 'chat_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'webchat',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'eyelikemeetingdb.cqtuldgsgw8c.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
         'NAME': 'webchat',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'USER': 'wlgud',
+        'PASSWORD': 'a79785920a*k0194757k!',
     }
 }
 
